c236164792d225874f7e379ec2a4f3e93a656b3e
akoskin <o.koskin@seznam.cz>
2018-08-09 11:12:44 +0200

    sdkapi-rabbit

---
+++ SdkBonusApi/array_setting.php
@@ -0,0 +1,58 @@
+<?php
+      $array_setting = array(
+
+         'server' => 'http://api.25one.com.ua/api_bonus.php', //when real SERVER - https://loyalty.fundist.org
+         'key' => '', //when real KEY (/.../)
+         'pwd' => 'password', //when real PASSWORD
+
+         '1' => array('addurl' => '/Loyalty/Levels/Set'), //'addurl' - PATH is already real
+         '2' => array('addurl' => '/Loyalty/Levels/Delete/'),
+         '3' => array('addurl' => '/Loyalty/Levels/Get/'),
+         '4' => array('addurl' => '/Bonuses/Set/'), 
+         '5' => array('addurl' => '/Bonuses/Data/'),
+         '6' => array('addurl' => '/Bonuses/Get/'),
+         '7' => array('addurl' => '/Bonuses/Select/'),
+         '8' => array('addurl' => '/Bonuses/Stats/'),
+         '9' => array('addurl' => '/Bonuses/Cancel/'), 
+         '10' => array('addurl' => '/Bonuses/Close/'),
+         '11' => array('addurl' => '/Bonuses/BalanceBlock/'),
+         '12' => array('addurl' => '/Bonuses/History/'),
+         '13' => array('addurl' => '/Bonuses/Delete/'),
+         '14' => array('addurl' => '/Loyalty/Set/'), 
+         '15' => array('addurl' => '/Loyalty/Get/'),
+         '16' => array('addurl' => '/Loyalty/Remove/'),
+         '17' => array('addurl' => '/Loyalty/Balance/[Type]'),   //!!![Type] - Type: Credit or Debet
+         '18' => array('addurl' => '/Events/Bundle/'),
+         '19' => array('addurl' => '/Store/Items/Set/'), 
+         '20' => array('addurl' => '/Store/Items/Status/'),
+         '21' => array('addurl' => '/Store/Items/Order/'),
+         '22' => array('addurl' => '/Store/Items/Delete/'),
+         '23' => array('addurl' => '/Store/Categories/Set/'),
+         '24' => array('addurl' => '/Store/Categories/Status/'), 
+         '25' => array('addurl' => '/Store/Categories/Order/'),
+         '26' => array('addurl' => '/Store/Categories/Delete/'),
+         '27' => array('addurl' => '/Store/Items/Relate/'),
+         '28' => array('addurl' => '/Store/Get/'),
+         '29' => array('addurl' => '/Store/Order/'), 
+         '30' => array('addurl' => '/Store/Orders/'),
+         '31' => array('addurl' => '/Store/Order/Process/'),
+         '32' => array('addurl' => '/Requests/Get/'),
+         '33' => array('addurl' => '/Requests/Status/'),
+         '34' => array('addurl' => '/Tournaments/Set/'), 
+         '35' => array('addurl' => '/Tournaments/Data/'),
+         '36' => array('addurl' => '/Tournaments/Get/'),
+         '37' => array('addurl' => '/Tournaments/Select/'),
+         '38' => array('addurl' => '/Tournaments/Delete/'),
+         '39' => array('addurl' => '/Tournaments/History/'), 
+         '40' => array('addurl' => '/Tournaments/Cancel/'),
+         '41' => array('addurl' => '/Tournaments/Widgets/[Type]/'),    //!!![Type] - Top – get tournament top players
+         '42' => array('addurl' => '/[Type]/Codes/IsValid/'),    //!!![Type] - Type: Bonuses or Tournaments   
+         '43' => array('addurl' => '/[Type]/Codes/Batch/'),      //!!![Type] - Type: Bonuses or Tournaments   
+         '44' => array('addurl' => '/[Type]/Codes/Generate/'),   //!!![Type] - Type: Bonuses or Tournaments   
+         '45' => array('addurl' => '/[Type]/Codes/Set/'),        //!!![Type] - Type: Bonuses or Tournaments   
+         '46' => array('addurl' => '/[Type]/Codes/Get/'),        //!!![Type] - Type: Bonuses or Tournaments   
+         '47' => array('addurl' => '/[Type]/Codes/Set/'),        //!!![Type] - Type: Bonuses or Tournaments    
+
+      );
+
+
