5288be797620c5858282bcac419bd06da0f75378
akoskin <o.koskin@seznam.cz>
2018-08-09 10:24:01 +0200

    sdkapi-rabbit

---
+++ README.md
@@ -0,0 +1,34 @@
+SdkBonusApi - sdk-class for loyalty_api_v106 from funidst.org + RABBIT-queue
+
+1.Installation.
+
+1.1. composer.json file:
+
+{
+    "require": {
+        "php":">=5.3.0",
+        "bonusapi-rabbit/sdkapi-rabbit": "dev-master"
+    },
+    "prefer-stable": true,
+    "minimum-stability": "dev"
+
+}
+
+1.2. composer install
+
+2.Basic usage.
+
+//...
+
+/*
+require_once 'vendor/autoload.php';
+
+use SdkBonusApi\SdkBonusApi;
+
+$bonusapi = new SdkBonusApi();
+
+$arr['numberapi'] = '1'; 
+$arr['postparamstring'] = 'TID=777/IDUser=55'; 
+
+$bonusapi->requestApi($arr);
+*/
---
+++ SdkBonusApi/SdkBonusApi.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SdkBonusApi;
+
+require_once 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use RabbitWrapper\WorkerSender;
+//use RabbitWrapper\WorkerReceiver;
+
+
+class SdkBonusApi
+{
+
+    public $array_setting; 
+
+    public function __construct() 
+    {
+
+      require_once 'array_setting.php'; //array_setting - general setting(server, key, pwd) + number api-items
+      $this->array_setting =  $array_setting;
+
+    }
+
+    public function requestApi($array_from_platform) //numberapi + postparamstring
+    {
+
+       //!!!guzzle... 
+       $array_params = explode('/', $array_from_platform['postparamstring']);
+       for($i = 0; $i < count($array_params); ++$i ) {$array_postparams[strstr($array_params[$i], '=', true)] = substr($array_params[$i], strpos($array_params[$i], '=')+1);}
+       $array_postparams['Hash'] = hash('md5', $array_from_platform['postparamstring'] . $this->array_setting['pwd']); 
+  
+       $client = new Client();
+
+       $response = $client->request('POST', $this->array_setting['server'].$this->array_setting['key'], [ //SERVER+KEY ...when real +PATH (.$this->array_setting[$array_from_platform['numberapi']]['addurl'])
+           'form_params' => [
+
+             //...for DEMO, when - delete...
+             'addurl' => $this->array_setting[$array_from_platform['numberapi']]['addurl'], //...PATH -> URL...
+             //...for DEMO, when - delete...
+
+             'parameters' => $array_postparams,    
+             
+           ]
+       ]);
+       echo $response->getStatusCode() . ' - ' . $response->getBody()->getContents();
+       //!!!guzzle... 
+
+       //!!!rabbit - send... 
+       $sender =  new WorkerSender();
+       $sender->execute('Hello world...'); 
+       //!!!rabbit - send... 
+          
+    }
+
+}
+
+
